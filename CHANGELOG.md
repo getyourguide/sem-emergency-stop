1147aac22ee2064796515d8f00dd2c1917e1f8c0
Paul Baecher <paul.baecher@getyourguide.com>
2021-03-24 16:06:49 +0100

    Add progress indicator
    Closes #6.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,20 @@
 # Changelog
 
+## 1.2.0
+
+Add progress indicator during operation.
+
+
+## 1.1.1
+
+Improve online instructions after pausing.
+
+
+## 1.1.0
+
+Upgrade to Google Ads API v6, minor fixes.
+
+
 ## 1.0.0
 
 Initial public release.
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='sem-emergency-stop',
-    version='1.1.1',
+    version='1.2.0',
     author='GetYourGuide GmbH',
     description='Quickly stop all Google Ads advertising',
     license='Apache License, Version 2.0',
