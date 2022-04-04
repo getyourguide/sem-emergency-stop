a2af633e830b9589256dd2203c4edd456dc8297d
Paul Baecher <paul.baecher@getyourguide.com>
2022-04-04 16:06:20 +0200

    Upgrade to Ads v10 for real

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='sem-emergency-stop',
-    version='1.3.1',
+    version='1.3.2',
     author='GetYourGuide GmbH',
     description='Quickly stop all Google Ads advertising',
     license='Apache License, Version 2.0',
